aa932069e67a7fba7c2a6068c43b775d3ae9e7f7
ordo-chao <princeben9312@gmail.com>
2025-04-12 16:13:04 +0300

    Homepage added

--- frontend/src/Components/Footer/index.jsx
+++ frontend/src/Components/Footer/index.jsx
@@ -1,9 +1,12 @@
 import styles from "./index.module.css";
 
 const Footer = () => {
+  const date = new Date();
+  console.log(date.getFullYear())
+  const year = date.getFullYear();
   return (
     <div className={styles.container}>
-      <h1>Footer</h1>
+      <p>&copy; {year} BookiFy. All rights reserved.</p>
     </div>
   );
 };
--- frontend/src/Screens/Home/index.jsx
+++ frontend/src/Screens/Home/index.jsx
@@ -5,7 +5,414 @@ const Home = () => {
   return (
     <Layout>
       <div className={styles.container}>
-        <h1>Home</h1>
+        {/* Trending books */}
+        <div className={styles.trending}>
+          <h1 className={styles.heading}>Trending books 🔥</h1>
+          <div className={styles.books}>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <p className={styles.viewmore}>View more >></p>
+          </div>
+        </div>
+
+        {/* By Genre / Subject */}
+        <div className={styles.trending}>
+          <h1 className={styles.heading}>Books for you 📖</h1>
+          <div className={styles.books}>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <p className={styles.viewmore}>View more >></p>
+          </div>
+        </div>
+
+        {/* On Sale / Discounted */}
+        <div className={styles.trending}>
+          <h1 className={styles.heading}>On Sale / Discounted 🏷️</h1>
+          <div className={styles.books}>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <p className={styles.viewmore}>View more >></p>
+          </div>
+        </div>
+
+        {/* New Arrivals books */}
+        <div className={styles.trending}>
+          <h1 className={styles.heading}>New Arrivals 🆕</h1>
+          <div className={styles.books}>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <p className={styles.viewmore}>View more >></p>
+          </div>
+        </div>
+
+        {/* Recommended Reads */}
+        <div className={styles.trending}>
+          <h1 className={styles.heading}>Recommended Reads 📌</h1>
+          <div className={styles.books}>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <p className={styles.viewmore}>View more >></p>
+          </div>
+        </div>
+
+
+        {/* Books for you books */}
+        <div className={styles.trending}>
+          <h1 className={styles.heading}>By Genre / Subject 🧠</h1>
+          <div className={styles.books}>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+            <div className={styles.book}>
+              <img src="src/assets/example.jpeg" alt="" className={styles.image} />
+              <p>Book: <span>48 Laws of Power</span> </p>
+              <p>Author: <span>Robert Greene</span></p>
+              <p>Price: <span>$10</span></p>
+              <p>Rating: <span>4.5</span></p>
+              <p>Pages: <span>400</span> </p>
+            </div>
+
+            <p className={styles.viewmore}>View more >></p>
+          </div>
+        </div>
       </div>
     </Layout>
   )
