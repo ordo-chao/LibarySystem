2ce2b4ea27e9d48caefcc74ab23e4a794a9bd44a
ordo-chao <princeben9312@gmail.com>
2025-04-12 15:29:34 +0300

    Navbar added

--- frontend/src/Components/Navbar/index.jsx
+++ frontend/src/Components/Navbar/index.jsx
@@ -1,11 +1,26 @@
-import styles from "./index.module.css";
+import styles from './index.module.css'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+function Navbar() {
+  const path = window.location.pathname;
   return (
     <div className={styles.container}>
-      <h1>Navbar</h1>
-    </div>
-  );
-};
+      <div className={styles.bookify}>
+        <span className={styles.logoicon}>📚</span>
+        <span className={styles.logotext}><span className={styles.highlight}>Booki</span>Fy</span>
+      </div>
 
-export default Navbar;
+      <div className={styles.links}>
+        <Link to="/" style={path === '/' ? { color: '#FF6D00' } : { color: '#0A3723' }}>Home</Link>
+        <Link to="/products" style={path === '/products' ? { color: '#FF6D00' } : { color: '#0A3723' }}>Search</Link>
+        <Link to="/about" style={path === '/about' ? { color: '#FF6D00' } : { color: '#0A3723' }}>About us</Link>
+        <Link to="/contact" style={path === '/contact' ? { color: '#FF6D00' } : { color: '#0A3723' }}>Order</Link>
+        <Link to="/education" style={path === '/education' ? { color: '#FF6D00' } : { color: '#0A3723' }}>Profile</Link>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default Navbar
