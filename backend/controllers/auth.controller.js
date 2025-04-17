edf2998a7af7ba2391540e51975fc7a9f3d82872
ordo-chao <princeben9312@gmail.com>
2025-04-17 04:09:01 +0300

    Google authentication added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 // controllers/auth.controller.js
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client("398653173102-qqb6km1sjdmqiur21vgkorfnrukcdppc.apps.googleusercontent.com");
+const userSchema = require("../Models/user.model");
 
 const googleLogin = async (req, res) => {
   const { idToken } = req.body;
@@ -12,12 +13,42 @@ const googleLogin = async (req, res) => {
     });
 
     const payload = ticket.getPayload();
-    res.json({ email: payload.email, name: payload.name });
+
+    const user = new userSchema(
+      {
+        name: payload.name,
+        email: payload.email,
+        password: generateRandomString(12),
+        role: 'user',
+        wishlist: req.body.wishlist,
+        orders: req.body.orders,
+        address: req.body.address,
+        profilePic: req.body.profilePic,
+        createdAt: req.body.createdAt
+      });
+    user.save();
+    console.log(user);
+    res.status(201).json({
+      success: true
+    });
   } catch (error) {
     console.error('Google Login Failed:', error);
     res.status(401).json({ error: 'Invalid token' });
   }
 };
+
+
+function generateRandomString(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+  let result = '';
+  
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+
+  return result;
+}
 
 
 module.exports = googleLogin;
